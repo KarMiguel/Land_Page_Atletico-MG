311863f4719e1ffdbcac52a161c37e1110fd42a8
KarMiguel <carlosmiguel.dsa12@gmail.com>
2023-12-19 22:34:25 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    $(".thumbnails").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        animateOut: 'slideOutLeft',
+        animateIn: 'slideInRight',
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        },
+        onInitialized: function() {
+            $(".thumbnails").find('.owl-stage').on('touchstart', function(e) {
+                if (e.type === 'touchstart') {
+                    e.preventDefault();
+                }
+            }, { passive: false });
+        }
+    });
+   
+    $("#btn-bars").on("click", function() {
+        $("header").toggleClass("open-menu");
+    });
+
+    $("#btnClose").on("click", function() {
+        $("header").removeClass("open-menu");
+    });
+    
+    
+    
+});
+
+$(".thumbnails").owlCarousel({
+    items:4
+});
+
+let owl = $(".thumbnails").data('owlCarousel');
+
+$("#btn-news-prev").on("click",function(){
+    owl.prev();
+});
+
+$("#btn-news-next").on("click",function(){
+    owl.next();
+});
+
+
